3016a59b606417bd217bc0e5b06c7f2a52828887
Donghyeon <drhon10@gmail.com>
2025-04-16 20:25:19 +0900

    필요한 모듈, GAS 추가

--- Source/Prologue/Prologue.Build.cs
+++ Source/Prologue/Prologue.Build.cs
@@ -8,6 +8,17 @@ public class Prologue : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "UMG" });
+		
+		PrivateDependencyModuleNames.AddRange(new string[] {
+			"GameFeatures",
+			"GameplayAbilities",
+			"GameplayTasks",
+			"GameplayTags",
+			"MotionWarping",
+			"AIModule",
+			"Niagara",
+			"AnimGraphRuntime"
+		});
 	}
 }
