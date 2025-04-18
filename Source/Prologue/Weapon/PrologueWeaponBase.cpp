f6c08f5a00c4d430b64d486b8e1e57c7614ba465
Donghyeon <drhon10@gmail.com>
2025-04-19 03:18:58 +0900

    InputAction 정리, Weapon 클래스 추가

---
+++ Source/Prologue/Weapon/PrologueWeaponBase.cpp
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PrologueWeaponBase.h"
+
+#include "Components/BoxComponent.h"
+
+
+// Sets default values
+APrologueWeaponBase::APrologueWeaponBase()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = false;
+
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+	SetRootComponent(WeaponMesh);
+
+	WeaponCollision = CreateDefaultSubobject<UBoxComponent>(TEXT("WeaponCollision"));
+	WeaponCollision->SetupAttachment(GetRootComponent());
+	WeaponCollision->SetBoxExtent(FVector(20.f));
+	WeaponCollision->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
