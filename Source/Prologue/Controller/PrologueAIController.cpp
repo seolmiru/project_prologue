3ff59c64b88e5e9083fb95e0b60a3a8f0c22174e
Donghyeon <drhon10@gmail.com>
2025-05-07 22:59:11 +0900

    EQS FindStrafing 추가

--- Source/Prologue/Controller/PrologueAIController.cpp
+++ Source/Prologue/Controller/PrologueAIController.cpp
@@ -33,7 +33,7 @@ ETeamAttitude::Type APrologueAIController::GetTeamAttitudeTowards(const AActor& 
 
 	const IGenericTeamAgentInterface* OtherTeamAgent = Cast<IGenericTeamAgentInterface>(PawnToCheck->GetController());
 
-	if (OtherTeamAgent && OtherTeamAgent->GetGenericTeamId() != GetGenericTeamId())
+	if (OtherTeamAgent && OtherTeamAgent->GetGenericTeamId() < GetGenericTeamId())
 	{
 		return ETeamAttitude::Hostile;
 	}
@@ -71,11 +71,14 @@ void APrologueAIController::BeginPlay()
 
 void APrologueAIController::OnEnemyPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus)
 {
-	if (Stimulus.WasSuccessfullySensed() && Actor)
+	if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
 	{
-		if (UBlackboardComponent* BlackboardComponent = GetBlackboardComponent())
+		if (!BlackboardComponent->GetValueAsObject(FName("TargetActor")))
 		{
-			BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+			if (Stimulus.WasSuccessfullySensed() && Actor)
+			{
+				BlackboardComponent->SetValueAsObject(FName("TargetActor"), Actor);
+			}
 		}
 	}
 }
