7a0653dd1755e89515034a604d9ac393367169d6
kdh <drhon10@gmail.com>
2025-06-17 07:08:51 +0900

    플레이어 사망 처리, 재시작 추가

---
+++ Source/Prologue/AbilitySystem/Ability/GA_CommaDeath.h
@@ -0,0 +1,29 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility.h"
+#include "GA_CommaDeath.generated.h"
+
+class ACommaController;
+class AComma;
+/**
+ * 
+ */
+UCLASS()
+class PROLOGUE_API UGA_CommaDeath : public UGameplayAbility
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure)
+	AComma* GetCommaCharacterFromActorInfo(); 
+
+	UFUNCTION(BlueprintPure)
+	ACommaController* GetCommaControllerFromActorInfo();
+
+private:
+	TWeakObjectPtr<AComma> CachedCommaCharacter;
+	TWeakObjectPtr<ACommaController> CachedCommaController;
+};
