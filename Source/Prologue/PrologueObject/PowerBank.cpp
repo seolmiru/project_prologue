4ef1b806e4eed1699599c2f6d678493824975002
kdh <drhon10@gmail.com>
2025-08-30 19:23:55 +0900

    NavLink 버그 수정

---
+++ Source/Prologue/PrologueObject/PowerBank.cpp
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PowerBank.h"
+
+#include "Components/BoxComponent.h"
+
+APowerBank::APowerBank()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>(TEXT("Root"));
+	
+	TriggerVolume = CreateDefaultSubobject<UBoxComponent>(TEXT("TriggerVolume"));
+	TriggerVolume->SetupAttachment(RootComponent);
+
+	PowerBankMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PowerBankMesh"));
+	PowerBankMesh->SetupAttachment(RootComponent);
+
+	TriggerVolume->OnComponentBeginOverlap.AddDynamic(this, &APowerBank::OnOverlapBegin);
+}
+
+void APowerBank::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	
+}
+
---
+++ Source/Prologue/PrologueObject/PowerBank.h
@@ -0,0 +1,28 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PowerBank.generated.h"
+
+class UBoxComponent;
+
+UCLASS()
+class PROLOGUE_API APowerBank : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	APowerBank();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trigger")
+	TObjectPtr<UBoxComponent> TriggerVolume;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Trigger")
+	TObjectPtr<UStaticMeshComponent> PowerBankMesh;
+
+	UFUNCTION()
+	void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
