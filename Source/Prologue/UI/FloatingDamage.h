5d0080dce5d24368f7af82f47edea910ecce0bc3
kdh <drhon10@gmail.com>
2025-08-31 22:07:08 +0900

    FloatingDamage 추가

---
+++ Source/Prologue/UI/FloatingDamage.h
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PrologueUserWidget.h"
+#include "Blueprint/UserWidget.h"
+#include "Prologue/AbilitySystem/Attribute/PrologueAttributeSet.h"
+#include "FloatingDamage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROLOGUE_API UFloatingDamage : public UPrologueUserWidget
+{
+	GENERATED_BODY()
+
+public:
+	virtual void SetAbilitySystemComponent(AActor* InOwner) override;
+
+	virtual void OnDamageChanged(const FOnAttributeChangeData& ChangeData);
+
+protected:
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadWrite, meta = (AllowPrivateAccess = true))
+	float Damage = 0.f;
+};
