3ea44c1d6da7807a1a311ec9eabafec07de27d47
Donghyeon <drhon10@gmail.com>
2025-09-26 15:36:14 +0900

    Clockwork Tick BP->C++, Merged Actor
    1. Clockwork BP에서 Tick 노드 수행하던 것들 전부 C++로 이동
    2. NewClock 배경 구조물 병합

---
+++ Source/Prologue/PrologueObject/Clockwork.cpp
@@ -0,0 +1,24 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Clockwork.h"
+
+AClockwork::AClockwork()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	ClockworkMesh = CreateDefaultSubobject<UStaticMeshComponent>("ClockworkMesh");
+	ClockworkMesh->SetupAttachment(GetRootComponent());
+}
+
+void AClockwork::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	const float PitchValue = RotationSpeed * DeltaTime;
+
+	const FRotator DeltaRotation = FRotator(PitchValue, 0.f, 0.f);
+
+	AddActorLocalRotation(DeltaRotation);
+}
+
