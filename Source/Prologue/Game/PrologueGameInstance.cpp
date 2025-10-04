1abd67a16391a26679b30a37ea58ac6bdc690eb8
kdh <drhon10@gmail.com>
2025-10-04 17:15:02 +0900

    아이콘 변경, 게임 실행 로딩 화면 변경

--- Source/Prologue/Game/PrologueGameInstance.cpp
+++ Source/Prologue/Game/PrologueGameInstance.cpp
@@ -168,6 +168,11 @@ void UPrologueGameInstance::RegisterCenterHub(ACenterHub* Hub)
 
 void UPrologueGameInstance::OnPreLoadMap(const FString& MapName)
 {
+	if (!bIsInitialLoadComplete)
+	{
+		return;
+	}
+	
 	FLoadingScreenAttributes LoadingScreenAttributes;
 	LoadingScreenAttributes.bAutoCompleteWhenLoadingCompletes = false;
 	LoadingScreenAttributes.MinimumLoadingScreenDisplayTime = 5.f;
@@ -179,6 +184,20 @@ void UPrologueGameInstance::OnPreLoadMap(const FString& MapName)
 void UPrologueGameInstance::OnDestinationWorldLoaded(UWorld* LoadedWorld)
 {
 	GetMoviePlayer()->StopMovie();
+
+	if (!bIsInitialLoadComplete && LoadedWorld)
+	{
+		FString MainMenuMapName = TEXT("MainMenuMap");
+		FString CurrentMapName = LoadedWorld->GetMapName();
+
+		LOG_SCREEN_R("Map Loaded : %s, Comparing : %s", *CurrentMapName, *MainMenuMapName);
+		
+		if (CurrentMapName == MainMenuMapName)
+		{
+			LOG_SCREEN_R("Detected");
+			bIsInitialLoadComplete = true;
+		}
+	}
 }
 
 TSharedPtr<SWidget> UPrologueGameInstance::CreateRandomLoadingWidget()
